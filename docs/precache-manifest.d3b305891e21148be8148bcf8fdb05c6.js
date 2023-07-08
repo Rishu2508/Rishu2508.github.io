b849e4ecbc307dd6e693fc1301c4144128b59254
Rishu Sagar <62638983+Rishu2508@users.noreply.github.com>
2023-07-09 05:12:23 +0530

    Add files via upload

---
+++ docs/precache-manifest.d3b305891e21148be8148bcf8fdb05c6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c3cd79515d6e3dca88f",
+    "url": "/portfolio/static/css/main.26bda5a0.chunk.css"
+  },
+  {
+    "revision": "5c3cd79515d6e3dca88f",
+    "url": "/portfolio/static/js/main.5c3cd795.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/portfolio/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "befcd5940a4aa37e57c5",
+    "url": "/portfolio/static/js/2.befcd594.chunk.js"
+  },
+  {
+    "revision": "c596cecd9157e80c9991",
+    "url": "/portfolio/static/js/runtime~main.c596cecd.js"
+  },
+  {
+    "revision": "6456e6f0098ce016f5484476d0a01723",
+    "url": "/portfolio/static/media/project-1.6456e6f0.png"
+  },
+  {
+    "revision": "7368421e8e6e703a011ce02a23ce4fc7",
+    "url": "/portfolio/static/media/project-2.7368421e.png"
+  },
+  {
+    "revision": "e4f93d09f1993b8242d6c8b5740ceca7",
+    "url": "/portfolio/static/media/project-3.e4f93d09.png"
+  },
+  {
+    "revision": "f9f48177c97e6fbef68a04e7241c1170",
+    "url": "/portfolio/static/media/me 3.f9f48177.png"
+  },
+  {
+    "revision": "27921383b7b4e3712d5faacc6d532e50",
+    "url": "/portfolio/static/media/School Logo.27921383.jpg"
+  },
+  {
+    "revision": "58e3f11eecfbe0875a260da74aa92ec5",
+    "url": "/portfolio/static/media/RBS Logo.58e3f11e.png"
+  },
+  {
+    "revision": "4a9f5434d5f21216a2fa1ed830c67514",
+    "url": "/portfolio/static/media/Dayalbagh Educational Institute Logo.4a9f5434.png"
+  },
+  {
+    "revision": "42416d96960c8ce897e1a6bcb9678c39",
+    "url": "/portfolio/static/media/project-square.42416d96.png"
+  },
+  {
+    "revision": "4e86019eb60336c5c98f17537a9481fe",
+    "url": "/portfolio/static/media/project-square-2.4e86019e.png"
+  },
+  {
+    "revision": "86f2de28822638ee5f3a24230b270db9",
+    "url": "/portfolio/static/media/Encoder Decoder.86f2de28.png"
+  },
+  {
+    "revision": "ed364309ec3a95cabe072df6780ab3e7",
+    "url": "/portfolio/static/media/dices.ed364309.png"
+  },
+  {
+    "revision": "44fa9cac03334fc066c44f416f3fe215",
+    "url": "/portfolio/static/media/TODO List.44fa9cac.png"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "/portfolio/static/media/Calibre-Regular-b0d4cbb1201155ddccc21d1b8059a670.b63c62e5.woff2"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "/portfolio/static/media/Calibre-Medium-568dce56f869a519a015d3b69443b067.46b36969.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "/portfolio/static/media/SFMono-Regular-8799e6387338d58f2f137df821c86eb4.7faa6660.woff2"
+  },
+  {
+    "revision": "54bf6f32a39334ed430bf10b1ac1851a",
+    "url": "/portfolio/static/media/Desktop Background(9).54bf6f32.png"
+  },
+  {
+    "revision": "8b5c01c19e8fe8774c1bc693cb1b8891",
+    "url": "/portfolio/static/media/green water splash.8b5c01c1.png"
+  },
+  {
+    "revision": "88ca56a293df1c51e479671315aed6be",
+    "url": "/portfolio/index.html"
+  }
+];
