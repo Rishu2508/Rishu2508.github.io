f6b61c2e203d963a376f200a459991016c6771cb
Rishu Sagar <62638983+Rishu2508@users.noreply.github.com>
2024-04-02 23:50:27 +0530

    Add files via upload

---
+++ docs/precache-manifest.61b6a60564a4186e947cb166f1382c26.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeb30dca7370f71266cc",
+    "url": "/static/css/main.3a2761b2.chunk.css"
+  },
+  {
+    "revision": "aeb30dca7370f71266cc",
+    "url": "/static/js/main.aeb30dca.chunk.js"
+  },
+  {
+    "revision": "adb2852fae2b928deb4c",
+    "url": "/static/js/1.adb2852f.chunk.js"
+  },
+  {
+    "revision": "108b46fca346b8931644",
+    "url": "/static/css/2.aeb72e6a.chunk.css"
+  },
+  {
+    "revision": "108b46fca346b8931644",
+    "url": "/static/js/2.108b46fc.chunk.js"
+  },
+  {
+    "revision": "ae34889c3d92eaa03e62",
+    "url": "/static/js/runtime~main.ae34889c.js"
+  },
+  {
+    "revision": "6456e6f0098ce016f5484476d0a01723",
+    "url": "/static/media/project-1.6456e6f0.png"
+  },
+  {
+    "revision": "4b607c74ee6f73e1d8f41225917876f3",
+    "url": "/static/media/chat app.4b607c74.png"
+  },
+  {
+    "revision": "e4f93d09f1993b8242d6c8b5740ceca7",
+    "url": "/static/media/project-3.e4f93d09.png"
+  },
+  {
+    "revision": "f9f48177c97e6fbef68a04e7241c1170",
+    "url": "/static/media/me 3.f9f48177.png"
+  },
+  {
+    "revision": "27921383b7b4e3712d5faacc6d532e50",
+    "url": "/static/media/School Logo.27921383.jpg"
+  },
+  {
+    "revision": "58e3f11eecfbe0875a260da74aa92ec5",
+    "url": "/static/media/RBS Logo.58e3f11e.png"
+  },
+  {
+    "revision": "4a9f5434d5f21216a2fa1ed830c67514",
+    "url": "/static/media/Dayalbagh Educational Institute Logo.4a9f5434.png"
+  },
+  {
+    "revision": "42416d96960c8ce897e1a6bcb9678c39",
+    "url": "/static/media/project-square.42416d96.png"
+  },
+  {
+    "revision": "4e86019eb60336c5c98f17537a9481fe",
+    "url": "/static/media/project-square-2.4e86019e.png"
+  },
+  {
+    "revision": "9ebfad016959a705bca2f01f4222678e",
+    "url": "/static/media/zackgudz.9ebfad01.png"
+  },
+  {
+    "revision": "3fd4dbbb076076e79cb24c518bd21639",
+    "url": "/static/media/RBS Logo square.3fd4dbbb.png"
+  },
+  {
+    "revision": "726d6a3aa3361b15e7f42e91c2612ed3",
+    "url": "/static/media/TODO-list.726d6a3a.png"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "/static/media/Calibre-Medium-568dce56f869a519a015d3b69443b067.46b36969.woff2"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "/static/media/Calibre-Regular-b0d4cbb1201155ddccc21d1b8059a670.b63c62e5.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "/static/media/SFMono-Regular-8799e6387338d58f2f137df821c86eb4.7faa6660.woff2"
+  },
+  {
+    "revision": "54bf6f32a39334ed430bf10b1ac1851a",
+    "url": "/static/media/Desktop Background(9).54bf6f32.png"
+  },
+  {
+    "revision": "d2490fe4d6578c8b7c622f51fd52b55a",
+    "url": "/index.html"
+  }
+];
